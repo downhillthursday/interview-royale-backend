cf3093e353315a6b675c733ca5d7023cd8d511f5
downhillthursday <adhirajmohanta@gmail.com>
2026-06-02 22:07:54 +0530

    connected mongo db

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,14 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db";
+
+dotenv.config();
 
 const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +17,8 @@ app.get("/", (_, res) => {
   res.send("Interview Royale Backend Running");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
